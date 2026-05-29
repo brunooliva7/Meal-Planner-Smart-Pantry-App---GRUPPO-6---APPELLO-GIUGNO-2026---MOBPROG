58ec2dc1e143c61993015cbc715c26db6bc70579
galactus-the-first-steps <d.vessa@studenti.unisa.it>
2026-05-29 12:09:45 +0200

    commit per la modifica del colore backgroung

--- weekbite/lib/main.dart
+++ weekbite/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:google_fonts/google_fonts.dart';
 import 'screens/dispensa.dart';
 import 'screens/ricette.dart';
 import 'screens/main_screen.dart';
+import 'screens/mealplanner.dart';
 
 // ==========================================================
 // ⚙️ CONFIGURAZIONI GLOBALI
