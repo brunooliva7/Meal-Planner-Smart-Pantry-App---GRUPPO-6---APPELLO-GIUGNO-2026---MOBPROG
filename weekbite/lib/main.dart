90513315c252b55ffdeefab2f0926baab7e01214
brux1o <bruno.oliva2004@gmail.com>
2026-05-29 12:15:28 +0200

    main modified

--- weekbite/lib/main.dart
+++ weekbite/lib/main.dart
@@ -79,7 +79,7 @@ class _BaseLayoutState extends State<BaseLayout> {
   // 📄 LE TUE SCHERMATE (QUI SOSTITUIRAI CON I TUOI FILE)
   // ==========================================================
   static final List<Widget> _pages = <Widget>[
-    const RicetteScreen(),
+    const MainScreen(),
     Center(child: Text("Meal Plan", style: GoogleFonts.montserrat(fontSize: 24, fontWeight: FontWeight.w600, color: Colors.black87))),
     Center(child: Text("Aggiungi", style: GoogleFonts.montserrat(fontSize: 24, fontWeight: FontWeight.w600, color: Colors.black87))),
     const DispensaScreen(),
