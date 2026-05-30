dd420045c1620aeff332e69535753be588cf901f
galactus-the-first-steps <d.vessa@studenti.unisa.it>
2026-05-30 19:05:09 +0200

    notifiche

--- weekbite/android/app/build.gradle.kts
+++ weekbite/android/app/build.gradle.kts
@@ -23,6 +23,8 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
+
+        isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
@@ -51,4 +53,8 @@ android {
 
 flutter {
     source = "../.."
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.3")
 }
